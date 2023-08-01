4638b6b195a27fd3a37eb08c54913f056814410a
Kamil Sulkowski <kamil.sulkowski98@gmail.com>
2023-08-01 12:42:05 +0200

    Przygotowanie podstwowych funkcji aplikacji toLow, toHight, equal, congratulation, genANum i zmiennych globalnych playerNumber, genNumber, attept

--- Zgadywator/Program.cs
+++ Zgadywator/Program.cs
@@ -1,72 +1,20 @@
 ﻿using System.Diagnostics;
+using System.Security.Cryptography;
 
 public class Zgadywator
 {
-    static int playerDec;
     static int playerNumber = 0;
     static int genNumber = 0;
     static int attempt = 0;
-    static Stopwatch stopwatch = new Stopwatch();
+
     public static void Main()
     {
-        Console.WriteLine("\nZGADYWATOR");
-        Console.WriteLine("Wybierz opcję:");
-        Console.WriteLine("1. Graj w zgadywator!");
-        Console.WriteLine("2. Sprawdź wyniki");
-        Console.WriteLine("3. Wyjdź");
-        Console.Write("Twój wybór: ");
-        if (int.TryParse(Console.ReadLine(), out playerDec))
-        {
-            switch (playerDec)
-            {
-                case 1:
-                    playGame();
-                    break;
-                case 2:
 
-                    break;
-                case 3:
-                    Console.WriteLine("Dzięki za gre");
-                    return;
-                default:
-                    Console.WriteLine("Niepoprawny wybór. Spróbuj ponownie.");
-                    break;
-            }
-        }
     }
 
     public static void playGame()
     {
-        genANum();
-        Console.WriteLine("\nWitaj w Zgadywatorze!");
-        Console.WriteLine("Liczba została wygenerowa.\nZgadnij liczbę od 1 do 100.\n");
 
-        stopwatch.Reset();
-        stopwatch.Start();
-
-        while (true)
-        {
-            Console.Write("Wpisz swoją liczbę: ");
-            try
-            {
-                playerNumber = Convert.ToInt32(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Błąd! Wpisz liczbę od 1 do 100");
-            }
-            attempt++;
-
-            toLow();
-            toHigh();
-            equal();
-            if (playerNumber == genNumber)
-            {
-                congratulation();
-                break;
-            }
-
-        }
     }
 
     public static void genANum()
@@ -104,7 +52,6 @@ public class Zgadywator
         Console.WriteLine("Gratulację wygrałeś");
         Console.WriteLine($"Odgadnięta liczba: {genNumber}");
         Console.WriteLine($"Liczba prób: {attempt}");
-        Console.WriteLine($"Czas: {stopwatch.Elapsed}\n");
     }
 }
  
