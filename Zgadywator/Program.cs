82cb3eab149f024a9de20dbd2437d656548c562d
Kamil Sulkowski <kamil.sulkowski98@gmail.com>
2023-08-02 10:20:26 +0200

    dodanie do pliku gameStats wykrywania najlepszego czasu, dodanie do Program funkcje loadStats, saveStats, displayStats oraz calculateAverageAttempts odpowiedzialne za kolejno wczytywanie statystk z pliku JSON zapisywanie do pliku JSON wypisywanie najlepszych statystyk po interakcji z użytkownikiem oraz obliczanie najlepszego podejścia

--- Zgadywator/Program.cs
+++ Zgadywator/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 public class Zgadywator
 {
     static int playerNumber = 0;
@@ -13,36 +12,67 @@ public class Zgadywator
 
     public static void Main()
     {
-        int playerDec;
-        Console.WriteLine("\nZGADYWATOR");
-        Console.WriteLine("Wybierz opcję:");
-        Console.WriteLine("1. Graj w zgadywator!");
-        Console.WriteLine("2. Sprawdź wyniki");
-        Console.WriteLine("3. Wyjdź");
-        Console.Write("Twój wybór: ");
-        if (int.TryParse(Console.ReadLine(), out playerDec))
-        {
-            switch (playerDec)
-            {
-                case 1:
-                    playGame();
-                    break;
-                case 2:
+        loadStats();
 
-                    break;
-                case 3:
-                    Console.WriteLine("Dzięki za gre");
-                    return;
-                default:
-                    Console.WriteLine("Niepoprawny wybór. Spróbuj ponownie.");
-                    break;
+        while (true)
+        {
+            Console.WriteLine("\nZGADYWATOR");
+            Console.WriteLine("Wybierz opcję:");
+            Console.WriteLine("1. Graj w zgadywator!");
+            Console.WriteLine("2. Sprawdź wyniki");
+            Console.WriteLine("3. Wyjdź");
+            Console.Write("Twój wybór: ");
+            if (int.TryParse(Console.ReadLine(), out int playerDec))
+            {
+                switch (playerDec)
+                {
+                    case 1:
+                        playGame();
+                        break;
+                    case 2:
+                        displayStats();
+                        break;
+                    case 3:
+                        Console.WriteLine("Dzięki za gre");
+                        SaveStats();
+                        return;
+                    default:
+                        Console.WriteLine("Niepoprawny wybór. Spróbuj ponownie.");
+                        break;
+                }
             }
         }
     }
 
+    public static void loadStats()
+    {
+        string baseFile = "stats.json";
+
+        if (File.Exists(baseFile))
+        {
+            string jsonStats = File.ReadAllText(baseFile);
+            gameStats = JsonConvert.DeserializeObject<GameStats>(jsonStats);
+        }
+    }
+    public static void SaveStats()
+    {
+        string baseFile = "stats.json";
+
+        gameStats.totalGames++;
+        gameStats.totalAttempts += attempt;
+        gameStats.bestScore = Math.Min(gameStats.bestScore, attempt);
+        gameStats.FastestTime = gameStats.FastestTime < stopwatch.Elapsed ? gameStats.FastestTime : stopwatch.Elapsed;
+
+        string jsonStats = JsonConvert.SerializeObject(gameStats, Formatting.Indented);
+
+        File.WriteAllText(baseFile, jsonStats);
+
+        Console.WriteLine("Statystyki zostały zapisane do pliku.");
+    }
     public static void playGame()
     {
         genANum();
+        attempt = 0;
         Console.WriteLine("\nWitaj w Zgadywatorze!");
         Console.WriteLine("Liczba została wygenerowa.\nZgadnij liczbę od 1 do 100.\n");
 
@@ -69,6 +99,7 @@ public class Zgadywator
             if (playerNumber == genNumber)
             {
                 congratulation();
+                SaveStats();
                 break;
             }
 
@@ -101,7 +132,7 @@ public class Zgadywator
     {
         if (playerNumber == genNumber)
         {
-            Console.WriteLine("Twoja liczba jest za równa wygenerowanej liczbie\n");
+            Console.WriteLine("Twoja liczba jest równa wygenerowanej liczbie\n");
         }
     }
 
@@ -111,6 +142,31 @@ public class Zgadywator
         Console.WriteLine($"Odgadnięta liczba: {genNumber}");
         Console.WriteLine($"Liczba prób: {attempt}");
         Console.WriteLine($"Czas: {stopwatch.Elapsed}\n");
+
+        // Sprawdzamy, czy czas wygranej jest najszybszy i go aktualizujemy
+        if (stopwatch.Elapsed < gameStats.FastestTime)
+        {
+            gameStats.FastestTime = stopwatch.Elapsed;
+            Console.WriteLine("Najszybszy czas wygranej!");
+        }
+    }
+
+    public static void displayStats()
+    {
+        Console.WriteLine("\nStatystyki:");
+        Console.WriteLine($"Liczba gier: {gameStats.totalGames}");
+        Console.WriteLine($"Średnia liczba prób: {calculateAverageAttempts():F2}");
+        Console.WriteLine($"Najlepszy wynik: {gameStats.bestScore}");
+        Console.WriteLine($"Najszybszy czas wygranej: {gameStats.FastestTime}\n");
+    }
+
+    public static double calculateAverageAttempts()
+    {
+        if (gameStats.totalGames > 0)
+        {
+            return (double)gameStats.totalAttempts / gameStats.totalGames;
+        }
+        return 0;
     }
 }
  
